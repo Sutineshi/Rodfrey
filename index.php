403101d907545e353e9e9e11ef316bfaefaa3197
Sutineshi <sutineshi@gmail.com>
2025-01-10 15:42:17 +0700

    Update and rename index.php to index.html

--- index.php
+++
@@ -1,8 +0,0 @@
-$token = "7769163596:AAEozTieX1YiP_jasM5vMTlsRpAA1Ejj5Gw";
-$chat_id = 508352253;
-
-$textMessage = "Тестовое сообщение";
-$textMessage = urlencode($textMessage);
-
-
-$urlQuery = "https://api.telegram.org/bot". $token ."/sendMessage?chat_id=". $chat_id ."&text=" . $textMessage;
