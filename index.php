65879e19cc2712f39fb95b6f2ac2510c4e5f0eb3
Sutineshi <sutineshi@gmail.com>
2025-01-10 15:38:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
-define("TG_TOKEN", "7769163596:AAEozTieX1YiP_jasM5vMTlsRpAA1Ejj5Gw")
-define("TG_USER_ID", "	508352253")
-
+$token = "7769163596:AAEozTieX1YiP_jasM5vMTlsRpAA1Ejj5Gw";
+$chat_id = 508352253;
 
 $textMessage = "Тестовое сообщение";
 $textMessage = urlencode($textMessage);
