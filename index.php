63b30cb239bfd6e1e9ec537aede6abaafb719fb8
Sutineshi <sutineshi@gmail.com>
2025-01-10 15:36:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,9 @@
+define("TG_TOKEN", "7769163596:AAEozTieX1YiP_jasM5vMTlsRpAA1Ejj5Gw")
+define("TG_USER_ID", "	508352253")
+
+
 $textMessage = "Тестовое сообщение";
 $textMessage = urlencode($textMessage);
+
+
+$urlQuery = "https://api.telegram.org/bot". $token ."/sendMessage?chat_id=". $chat_id ."&text=" . $textMessage;
